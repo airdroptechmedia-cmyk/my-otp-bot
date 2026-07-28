461248d3e0c0edab11f95f8413d19ffa4f69b335
airdroptechmedia-cmyk <airdroptechmedia@gmail.com>
2026-07-28 09:55:51 +0600

    Update main.py

--- main.py
+++ main.py
@@ -231,7 +231,7 @@ def start_cmd(message):
                 cursor.execute("UPDATE users SET referrals = referrals + 1 WHERE user_id=?", (referred_by,))
             conn.commit()
 
-    bot.send_message(message.chat.id, "👋 <b>Will be Earn Shop & Fast SMS Bot</b>-এ আপনাকে স্বাগতম!", reply_markup=main_reply_keyboard(user_id))
+    bot.send_message(message.chat.id, "👋 <b>OTP RECIVER PRO BOT</b>-এ আপনাকে স্বাগতম!", reply_markup=main_reply_keyboard(user_id))
 
 # ----------------- 📱 NUMBER'S BOT SYSTEM -----------------
 @bot.message_handler(func=lambda msg: msg.text in ["📱 Get Number", "📱 NUMBER'S"])
